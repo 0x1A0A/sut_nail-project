7c3baf4d0123789c3b89db22b134120c8496ee73
0x1A0A <mask.monkey1994@gmail.com>
2022-11-09 14:52:00 +0700

    remove build file from repo.

--- cpp/build/vendors/raylib-4.2.0/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/defalse/.Null/SUT/RA/raycv/cpp/vendors/raylib-4.2.0/src
-# Build directory: /home/defalse/.Null/SUT/RA/raycv/cpp/build/vendors/raylib-4.2.0/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
